461e1673844120d3edb8286d7a838527e3561bdf
up202107596 <up202107596@edu.fc.up.pt>
2023-11-15 22:54:04 +0000

    add the logger system

--- jersey/myapp/src/main/java/fcup/pdm/myapp/api/RegisterResource.java
+++ jersey/myapp/src/main/java/fcup/pdm/myapp/api/RegisterResource.java
@@ -4,6 +4,9 @@ import fcup.pdm.myapp.model.User;
 import fcup.pdm.myapp.dao.UserDAO;
 import fcup.pdm.myapp.util.PasswordUtil;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -15,19 +18,25 @@ import jakarta.ws.rs.core.Response;
 
 @Path("/register")
 public class RegisterResource {
+    private static final Logger logger = LogManager.getLogger(RegisterResource.class);
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response registerUser(User user) {
+
+        logger.debug("Attempting to register user: " + user.getUsername());
+
         UserDAO userDao = new UserDAO();
         user.setHashedPassword(PasswordUtil.hashPassword(user.getPassword()));
 
         if (userDao.addUser(user)) {
             // Registration successful
+            logger.info("Registration successful for user: " + user.getUsername());
             return Response.ok().entity("Registration Successful").build();
         } else {
             // Registration failed
+            logger.info("Registration successful for user: " + user.getUsername());
             return Response.status(Response.Status.BAD_REQUEST).entity("Registration Failed").build();
         }
     }
