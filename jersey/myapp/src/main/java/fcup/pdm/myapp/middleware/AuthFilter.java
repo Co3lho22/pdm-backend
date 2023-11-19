fc177d16645af0ce9a2a9a6490038228f52f198f
up202107596 <up202107596@edu.fc.up.pt>
2023-11-19 19:30:46 +0000

    try /register without the filter

--- jersey/myapp/src/main/java/fcup/pdm/myapp/middleware/AuthFilter.java
+++ jersey/myapp/src/main/java/fcup/pdm/myapp/middleware/AuthFilter.java
@@ -1,35 +1,35 @@
-package fcup.pdm.myapp.middleware;
-
-import fcup.pdm.myapp.util.JwtUtil;
-import jakarta.ws.rs.container.ContainerRequestContext;
-import jakarta.ws.rs.container.ContainerRequestFilter;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.ext.Provider;
-import java.net.URI;
-
-@Provider
-public class AuthFilter implements ContainerRequestFilter {
-
-    @Override
-    public void filter(ContainerRequestContext requestContext) {
-        URI requestUri = requestContext.getUriInfo().getRequestUri();
-        String path = requestUri.getPath();
-
-        if (path.endsWith("/login") || path.endsWith("/register")) {
-            return;
-        }
-
-        String authHeader = requestContext.getHeaderString("Authorization");
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("Authorization header must be provided").build());
-            return;
-        }
-
-        String token = authHeader.substring("Bearer ".length()).trim();
-        try {
-            JwtUtil.verifyToken(token);
-        } catch (Exception e) {
-            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("Invalid token").build());
-        }
-    }
-}
+//package fcup.pdm.myapp.middleware;
+//
+//import fcup.pdm.myapp.util.JwtUtil;
+//import jakarta.ws.rs.container.ContainerRequestContext;
+//import jakarta.ws.rs.container.ContainerRequestFilter;
+//import jakarta.ws.rs.core.Response;
+//import jakarta.ws.rs.ext.Provider;
+//import java.net.URI;
+//
+//@Provider
+//public class AuthFilter implements ContainerRequestFilter {
+//
+//    @Override
+//    public void filter(ContainerRequestContext requestContext) {
+//        URI requestUri = requestContext.getUriInfo().getRequestUri();
+//        String path = requestUri.getPath();
+//
+//        if (path.endsWith("/login") || path.endsWith("/register")) {
+//            return;
+//        }
+//
+//        String authHeader = requestContext.getHeaderString("Authorization");
+//        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+//            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("Authorization header must be provided").build());
+//            return;
+//        }
+//
+//        String token = authHeader.substring("Bearer ".length()).trim();
+//        try {
+//            JwtUtil.verifyToken(token);
+//        } catch (Exception e) {
+//            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("Invalid token").build());
+//        }
+//    }
+//}
