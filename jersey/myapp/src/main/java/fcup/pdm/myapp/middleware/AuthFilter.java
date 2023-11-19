e98c262112f8c346987e36a97e688c0acb472993
up202107596 <up202107596@edu.fc.up.pt>
2023-11-19 19:23:56 +0000

    add the logic to verify that each endpoint has the token unless /login and /register

--- jersey/myapp/src/main/java/fcup/pdm/myapp/middleware/AuthFilter.java
+++ jersey/myapp/src/main/java/fcup/pdm/myapp/middleware/AuthFilter.java
@@ -5,12 +5,20 @@ import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
+import java.net.URI;
 
 @Provider
 public class AuthFilter implements ContainerRequestFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext) {
+        URI requestUri = requestContext.getUriInfo().getRequestUri();
+        String path = requestUri.getPath();
+
+        if (path.endsWith("/login") || path.endsWith("/register")) {
+            return;
+        }
+
         String authHeader = requestContext.getHeaderString("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("Authorization header must be provided").build());
