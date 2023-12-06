ca22cb2fb835a0f68da67afc9ab9f2a3d493a0e2
up202107596 <up202107596@edu.fc.up.pt>
2023-12-06 21:48:05 +0000

    update the adminDAO

--- jersey/myapp/src/main/java/fcup/pdm/myapp/dao/AdminDAO.java
+++ jersey/myapp/src/main/java/fcup/pdm/myapp/dao/AdminDAO.java
@@ -17,6 +17,112 @@ public class AdminDAO {
     private static final Logger logger = LogManager.getLogger(AdminDAO.class);
 
     /**
+     * Removes a user from the database by their ID and deletes related data.
+     *
+     * @param userId The ID of the user to be removed.
+     * @return True if the user was removed successfully; otherwise, false.
+     */
+    public boolean removeUser(int userId) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        try {
+            connection = DBConnection.getConnection();
+            connection.setAutoCommit(false);
+
+            String deleteUserRolesQuery = "DELETE FROM USER_ROLE WHERE user_id = ?";
+            ps = connection.prepareStatement(deleteUserRolesQuery);
+            ps.setInt(1, userId);
+            ps.executeUpdate();
+
+            String deleteFavoritesMoviesQuery = "DELETE FROM USER_FAVORITES_MOVIES WHERE user_id = ?";
+            ps = connection.prepareStatement(deleteFavoritesMoviesQuery);
+            ps.setInt(1, userId);
+            ps.executeUpdate();
+
+            String deleteFavoritesGenresQuery = "DELETE FROM USER_FAVORITES_GENRES WHERE user_id = ?";
+            ps = connection.prepareStatement(deleteFavoritesGenresQuery);
+            ps.setInt(1, userId);
+            ps.executeUpdate();
+
+            String deleteAuthQuery = "DELETE FROM USER_AUTH WHERE user_id = ?";
+            ps = connection.prepareStatement(deleteAuthQuery);
+            ps.setInt(1, userId);
+            ps.executeUpdate();
+
+            String deleteUserQuery = "DELETE FROM USERS WHERE id = ?";
+            ps = connection.prepareStatement(deleteUserQuery);
+            ps.setInt(1, userId);
+            int rowsAffected = ps.executeUpdate();
+
+            if (rowsAffected == 0) {
+                connection.rollback();
+                return false;
+            }
+
+            connection.commit();
+            logger.info("Removed user successfully with userID: {}", userId);
+            return true;
+        } catch (Exception e) {
+            logger.error("Error removing user with userID: {}", userId, e);
+
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            return false;
+        } finally {
+            closeResources(ps, connection);
+        }
+    }
+
+    /**
+     * Adds a new genre to the database.
+     *
+     * @param genreName The name of the genre to be added.
+     * @return True if the genre was added successfully; otherwise, false.
+     */
+    public boolean addGenre(String genreName) {
+        String query = "INSERT INTO GENRES (name) VALUES (?)";
+        try (Connection connection = DBConnection.getConnection();
+             PreparedStatement ps = connection.prepareStatement(query)) {
+
+            ps.setString(1, genreName);
+            int rowsAffected = ps.executeUpdate();
+
+            logger.info("Genre added successfully with name: {}", genreName);
+            return rowsAffected > 0;
+        } catch (Exception e) {
+            logger.error("Error adding genre: {}", genreName, e);
+            return false;
+        }
+    }
+
+    /**
+     * Removes a genre from the database by its ID.
+     *
+     * @param genreId The ID of the genre to be removed.
+     * @return True if the genre was removed successfully; otherwise, false.
+     */
+    public boolean removeGenre(int genreId) {
+        String query = "DELETE FROM GENRES WHERE id = ?";
+        try (Connection connection = DBConnection.getConnection();
+             PreparedStatement ps = connection.prepareStatement(query)) {
+
+            ps.setInt(1, genreId);
+            int rowsAffected = ps.executeUpdate();
+
+            logger.info("Genre removed successfully with ID: {}", genreId);
+            return rowsAffected > 0;
+        } catch (Exception e) {
+            logger.error("Error removing genre with ID: {}", genreId, e);
+            return false;
+        }
+    }
+
+    /**
      * Adds a new movie and links it to a genre.
      *
      * @param movie   The movie object to be added.
@@ -61,8 +167,6 @@ public class AdminDAO {
                 connection.rollback();
                 return false;
             }
-
-            // connection.commit();
 
             return true;
         } catch (Exception e) {
@@ -158,50 +262,6 @@ public class AdminDAO {
     }
 
     /**
-     * Adds a new genre to the database.
-     *
-     * @param genreName The name of the genre to be added.
-     * @return True if the genre was added successfully; otherwise, false.
-     */
-    public boolean addGenre(String genreName) {
-        String query = "INSERT INTO GENRES (name) VALUES (?)";
-        try (Connection connection = DBConnection.getConnection();
-             PreparedStatement ps = connection.prepareStatement(query)) {
-
-            ps.setString(1, genreName);
-            int rowsAffected = ps.executeUpdate();
-
-            logger.info("Genre added successfully with name: {}", genreName);
-            return rowsAffected > 0;
-        } catch (Exception e) {
-            logger.error("Error adding genre: {}", genreName, e);
-            return false;
-        }
-    }
-
-    /**
-     * Removes a genre from the database by its ID.
-     *
-     * @param genreId The ID of the genre to be removed.
-     * @return True if the genre was removed successfully; otherwise, false.
-     */
-    public boolean removeGenre(int genreId) {
-        String query = "DELETE FROM GENRES WHERE id = ?";
-        try (Connection connection = DBConnection.getConnection();
-             PreparedStatement ps = connection.prepareStatement(query)) {
-
-            ps.setInt(1, genreId);
-            int rowsAffected = ps.executeUpdate();
-
-            logger.info("Genre removed successfully with ID: {}", genreId);
-            return rowsAffected > 0;
-        } catch (Exception e) {
-            logger.error("Error removing genre with ID: {}", genreId, e);
-            return false;
-        }
-    }
-
-    /**
      * Links a movie with a genre in the database.
      *
      * @param movieId The ID of the movie to be linked.
@@ -224,93 +284,6 @@ public class AdminDAO {
             return false;
         }
     }
-
-    /**
-     * Unlinks a movie from a genre in the database.
-     *
-     * @param movieId The ID of the movie to be unlinked.
-     * @param genreId The ID of the genre to unlink the movie from.
-     * @return True if the movie was unlinked from the genre successfully; otherwise, false.
-     */
-    private boolean unlinkMovieFromGenre(int movieId, int genreId) {
-        String query = "DELETE FROM MOVIE_GENRES WHERE movie_id = ? AND genre_id = ?";
-        try (Connection connection = DBConnection.getConnection();
-             PreparedStatement ps = connection.prepareStatement(query)) {
-
-            ps.setInt(1, movieId);
-            ps.setInt(2, genreId);
-            int rowsAffected = ps.executeUpdate();
-
-            logger.info("Unlinked movie with id={} with genre with id={} successfully", movieId, genreId);
-            return rowsAffected > 0;
-        } catch (Exception e) {
-            logger.error("Error unlinking movie with id={} with genre with id={}", movieId, genreId, e);
-            return false;
-        }
-    }
-
-    /**
-     * Removes a user from the database by their ID and deletes related data.
-     *
-     * @param userId The ID of the user to be removed.
-     * @return True if the user was removed successfully; otherwise, false.
-     */
-    public boolean removeUser(int userId) {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        try {
-            connection = DBConnection.getConnection();
-            connection.setAutoCommit(false);
-
-            String deleteUserRolesQuery = "DELETE FROM USER_ROLE WHERE user_id = ?";
-            ps = connection.prepareStatement(deleteUserRolesQuery);
-            ps.setInt(1, userId);
-            ps.executeUpdate();
-
-            String deleteFavoritesMoviesQuery = "DELETE FROM USER_FAVORITES_MOVIES WHERE user_id = ?";
-            ps = connection.prepareStatement(deleteFavoritesMoviesQuery);
-            ps.setInt(1, userId);
-            ps.executeUpdate();
-
-            String deleteFavoritesGenresQuery = "DELETE FROM USER_FAVORITES_GENRES WHERE user_id = ?";
-            ps = connection.prepareStatement(deleteFavoritesGenresQuery);
-            ps.setInt(1, userId);
-            ps.executeUpdate();
-
-            String deleteAuthQuery = "DELETE FROM USER_AUTH WHERE user_id = ?";
-            ps = connection.prepareStatement(deleteAuthQuery);
-            ps.setInt(1, userId);
-            ps.executeUpdate();
-
-            String deleteUserQuery = "DELETE FROM USERS WHERE id = ?";
-            ps = connection.prepareStatement(deleteUserQuery);
-            ps.setInt(1, userId);
-            int rowsAffected = ps.executeUpdate();
-
-            if (rowsAffected == 0) {
-                connection.rollback();
-                return false;
-            }
-
-            connection.commit();
-            logger.info("Removed user successfully with userID: {}", userId);
-            return true;
-        } catch (Exception e) {
-            logger.error("Error removing user with userID: {}", userId, e);
-
-            if (connection != null) {
-                try {
-                    connection.rollback();
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
-            }
-            return false;
-        } finally {
-            closeResources(ps, connection);
-        }
-    }
-
 
     /**
      * Closes the prepared statement and database connection.
